f53ca755073d8a6f8beb6551f772b4aa7ecc3f4f
ottocesc <ottocesc@gmail.com>
2020-03-06 17:08:21 +0100

    Using setState done, pass comes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+
+
+let bookList = [
+  {"title": "The Sun", "author": "Ernest Hemingway", "pages": 260},
+  {"title": "Un Capitano", "author": "Francesco Totti", "pages": 560},
+  {"title": "Lord of the rings 1", "author": "JJR Tolk", "pages": 266},
+  {"title": "Lord of the rings 2", "author": "JJR Tolk", "pages": 266},
+  {"title": "Lord of the rings 3", "author": "JJR Tolk", "pages": 266}
+]
 //import './index.css';
 //import App from './App';
 //import * as serviceWorker from './serviceWorker';
@@ -7,23 +16,44 @@ const Book = ({title, author, pages})  => {
   return (
     <section>
       <h2>{title}</h2>
-      <h2>by: {author}</h2>
-      <h2>Pages: {pages} pages</h2>
+      <p>by: {author}</p>
+      <p>Pages: {pages} pages</p>
     </section>
   )
 }
 
-const Library = ()  => {
-  return (
-    <div>
-      <Book title="The Sun Rises" author="Ernest Hemingway" pages={260}/>
-    </div>
-  )
+class Library extends React.Component {
+  state = { open: false }
+
+  toggleOpenClosed = () => {
+  this.setState(prevState => ({
+    open: !prevState.open
+  }))
+
+  }
+  render() {
+    console.log(this.state)
+    const { books } = this.props
+    return (
+      <div>
+      <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
+      <button onClick={this.toggleOpenClosed}>Change</button>
+        {books.map(
+          (book, i) => <Book
+                        key={i}
+                        title={book.title}
+                        author={book.author}
+                        pages={book.author}/>
+        )}
+      </div>
+    )
+  }
 }
+
 
 render(
   <div>
-    <Library />
+    <Library books={bookList} />
   </div>
   , document.getElementById('root'));
 
